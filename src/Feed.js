568a2cbfe91570720854000f41656e971ee281a0
Peter <sb1285n@gmail.com>
2021-01-14 17:08:23 -0500

    Added Post UI component and CSS for the Post header and body

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
+import Post from './Post'
 import './Feed.css'
 
 function Feed() {
@@ -8,7 +9,27 @@ function Feed() {
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      {/* MessageSender */}
+
+      <Post 
+        profilePic="./avatar.png"
+        message="This is a post."
+        timestamp="Timestamp"
+        username="Brotgils"
+        image="./portalIn.jpeg"
+      />
+      <Post 
+        profilePic="./avatar.png"
+        message="This is another post."
+        timestamp="Timestamp"
+        username="Brotgils"
+        image="./portalOut.jpeg"
+      />
+      <Post 
+        profilePic="./avatar.png"
+        message="This is the final post."
+        timestamp="Timestamp"
+        username="Brotgils"
+      />
     </div>
   )
 }
