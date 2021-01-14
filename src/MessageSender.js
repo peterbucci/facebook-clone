3592752ce29130ff365e4c78c3f86ec5adabbf6e
Peter <sb1285n@gmail.com>
2021-01-14 16:27:10 -0500

    Added MessageSender CSS for top and bottom header

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Avatar } from '@material-ui/core'
+import VideocamIcon from '@material-ui/icons/Videocam'
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import './MessageSender.css'
 
 function MessageSender() {
@@ -24,7 +27,18 @@ function MessageSender() {
         </form>
       </div>
       <div className="messageSender__bottom">
-
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
       </div>
     </div>
   )
