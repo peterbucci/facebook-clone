c471752fd1301c338c3cfcfb44b2bf9c2b05c999
Peter <sb1285n@gmail.com>
2023-03-09 12:56:38 -0500

    Updated login page

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,30 +1,20 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
-import './styles/login.css'
+import React from "react";
+import { Button } from "@material-ui/core";
+import "./styles/login.css";
 
-import { auth, googleProvider } from 'firebase.js'
+import { auth, googleProvider } from "firebase.js";
 
 export default function Login() {
   const signIn = (provider) => {
-    auth.signInWithPopup(provider)
-        .catch(error => alert(error.message))    
-  }
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
-      <div className="login__logo">
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" 
-          alt="facebook logo icon"
-        />
-        <img 
-          src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg"
-          alt="facebook logo text"
-        />
-      </div>
+      <div className="login__logo">A Facebook Clone</div>
       <Button type="submit" onClick={() => signIn(googleProvider)}>
         Sign In
       </Button>
     </div>
-  )
+  );
 }
